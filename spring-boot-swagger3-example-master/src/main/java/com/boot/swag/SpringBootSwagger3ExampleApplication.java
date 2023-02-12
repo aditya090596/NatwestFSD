9109689add206a75a6cfb3505648b10096611add
aditya090596 <122466845+aditya090596@users.noreply.github.com>
2023-02-12 22:45:33 +0530

    Add files via upload

---
+++ spring-boot-swagger3-example-master/src/main/java/com/boot/swag/SpringBootSwagger3ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.boot.swag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwagger3ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger3ExampleApplication.class, args);
+	}
+}
---
+++ spring-boot-swagger3-example-master/src/main/java/com/boot/swag/controller/Swagger3RestController.java
@@ -0,0 +1,44 @@
+package com.boot.swag.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.swag.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequestMapping("/api")
+@RestController
+public class Swagger3RestController {
+
+	List<User> users = new ArrayList<User>();
+	{
+		users.add(new User(1,"User1", "ADMIN", "user1@test.com"));
+		users.add(new User(2,"User2", "SUPERVISOR", "user2@test.com"));
+		users.add(new User(3,"User3", "USER", "user3@test.com"));
+		users.add(new User(4,"User4", "USER", "user4@test.com"));
+	}
+
+
+	@GetMapping("/getUsers")
+//	@RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = "application/json")
+	public List<User> getUsers() {
+		return users;
+	}
+
+	@RequestMapping(value = "/getUser/{id}", method = RequestMethod.GET, produces = "application/json")
+	public User getUserById(@PathVariable(value = "id") int id) {
+		return users.stream().filter(x -> x.getId()==(id)).collect(Collectors.toList()).get(0);
+	}
+
+	@RequestMapping(value = "/getUser/role/{role}", method = RequestMethod.GET, produces = "application/json")
+	public List<User> getUserByRole(@PathVariable(value = "role") String role) {
+		return users.stream().filter(x -> x.getRole().equalsIgnoreCase(role))
+				.collect(Collectors.toList());
+	}
+}
